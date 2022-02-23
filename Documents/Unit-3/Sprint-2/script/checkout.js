a8999dec154590dff7d0e5a0aa69b870f2b0e822
Abhishek-Choubey <96122002+Abhishek-Choubey@users.noreply.github.com>
2022-02-23 15:50:54 +0530

    first commit

--- Documents/Unit-3/Sprint-2/script/checkout.js
+++
@@ -1,9 +0,0 @@
-function check(){
-    var add = document.getElementById("address").value;  
-    localStorage.setItem("Address", JSON.stringify(add));
-    window.location.href = "/thank.html"
-    let id = setTimeout(function(){
-        alert("Your order is ready");
-       }, 10000);
-
-}
