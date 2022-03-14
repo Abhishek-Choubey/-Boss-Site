09e0516c61794e024231ddda4ddf55228ea9fe0b
Abhishek-Choubey <96122002+Abhishek-Choubey@users.noreply.github.com>
2022-03-14 10:55:32 +0530

    commited

---
+++ Documents/Masai_HTML/Unit-5/mockapp/src/Components/AddHouse/AddHouse.jsx
@@ -0,0 +1,87 @@
+import { axios } from 'axios';
+import { useEffect, useState } from "react";
+
+export const AddHouse = () => {
+  const [formData, setFormdata] = useState({
+    name: "",
+    ownerName: "",
+    address: "",
+    areaCode: "",
+    rent: "",
+    bachelor: "",
+    married: "",
+    image: "",
+
+  });
+  const [datas, setDatas] = useState([]);
+
+  function handleChange(e) {
+    const { className, value } = e.target;
+    setFormdata({...formData, [className]: value })
+  }
+  console.log("form",formData)
+
+  // useEffect(() => {
+  //   getData()
+   
+  // }, [])
+
+  function getData() {
+    axios.get("http://localhost:8080/houses").then((res) => {
+      setDatas(res.data);
+    })
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios.post("http://localhost:8080/houses", formData).then(() => {
+      alert("House added");
+      getData()      
+
+      setFormdata({
+        name: "",
+        ownerName: "",
+        address: "",
+        areaCode: "",
+        rent: "",
+        bachelor: "",
+        married: "",
+        image: "",
+
+      })
+    })
+
+  }
+  return (
+    <div className="addHouseContainer">
+      <form onSubmit={handleSubmit}>
+        <label>name</label>
+        <input type="text" onChange = {handleChange} className="name" value={formData.name} required />
+        <br />
+        <label>ownerName</label>
+        <input value={formData.ownerName} type="text" onChange = {handleChange} className="ownerName" required />
+        <br />
+        <label>address</label>
+        <input value={formData.address} type="text" onChange = {handleChange} className="address" required />
+        <br />
+        <label>areaCode</label>
+        <input value={formData.areaCode} type="text" onChange = {handleChange} className="areaCode" required />
+        <br />
+        <label>rent</label>
+        <input value={formData.rent} type="text" onChange = {handleChange} className="rent" required />
+        <br />
+        <label>preferredTenant</label>
+        <br />
+        <label>bachelor</label>
+        <input checked={formData.bachelor} type="checkbox" onChange = {handleChange} className="bachelor" />
+        <br />
+        <label>married</label>
+        <input checked={formData.married} type="checkbox" onChange = {handleChange} className="married" />
+        <br />
+        <label>image</label>
+        <input value={formData.image} type="text" onChange = {handleChange} className="image" required />
+        <br />
+        <input className="submitBtn" type="submit" />
+      </form>
+    </div>
+  );
+};
