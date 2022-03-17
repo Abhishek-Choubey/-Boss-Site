a2e4a0dd459f1c8d7e3da94fe48f4f6ced687f15
Abhishek-Choubey <96122002+Abhishek-Choubey@users.noreply.github.com>
2022-03-17 12:43:13 +0530

    committed

---
+++ Documents/Portfolio/src/data/headerData.js
@@ -0,0 +1,9 @@
+import resume from '../assets/pdf/resume.pdf'
+
+export const headerData = {
+    name: 'Abhishek Choubey',
+    title: "Full Stack Web Developer",
+    desciption:"Things do not happen Things are made to happen.",
+    image: 'https://cloud1.ipupload.com/cache/plugins/filepreviewer/14815/c87fa1bc230c59dad98c9d4de85ffc03749b9f0a9de33bc9a42691292abe04fd/1100x800_cropped.jpg',
+    resumePdf: resume
+}
